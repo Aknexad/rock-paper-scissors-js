7da2e1b3f8275c9d52c712d7a6bb5944c46c6a39
Aknexad <aknejad@outlook.com>
2022-06-19 11:15:22 +0430

    add computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+let option = ['Rock', 'Paper', 'Scissors'];
+
+let playerSelect = 'Paper';
+let computerSelection = computerPlay(option);
+
+function computerPlay(c) {
+  let randumNumber = Math.floor(Math.random() * 3);
+  return c[randumNumber];
+}
+
+console.log(`player  ${playerSelect}`);
+console.log(`computer ${computerSelection}`);
