9602fd34a57fcdd7d5457f33ed63de45761caf29
Aknexad <aknejad@outlook.com>
2022-06-19 11:42:15 +0430

    add Comparison function

--- script.js
+++ script.js
@@ -8,5 +8,36 @@ function computerPlay(c) {
   return c[randumNumber];
 }
 
+function comparison(ps, cs) {
+  if (ps !== cs) {
+    if (ps === 'Rock') {
+      if (cs == 'Paper') {
+        return `Lose ${cs} beats ${ps}`;
+      } else {
+        return 'Wine';
+      }
+    }
+    if (ps === 'Paper') {
+      if (cs == 'Scissors') {
+        return `Lose ${cs} beats ${ps}`;
+      }
+    } else {
+      return 'Wine';
+    }
+    if (ps === 'Scissors') {
+      if (cs == 'Rock') {
+        return `Lose ${cs} beats ${ps}`;
+      }
+    } else {
+      return 'Wine';
+    }
+  } else {
+    return 'Drow';
+  }
+}
+
 console.log(`player  ${playerSelect}`);
 console.log(`computer ${computerSelection}`);
+console.log(
+  `reult ${comparison(playerSelect, computerSelection)}`
+);
